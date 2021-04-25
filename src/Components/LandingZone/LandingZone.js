2f9bc8eb217c126635d08ab9bb5c1450d99d4087
Sean Dwyer <spdwyer06@gmail.com>
2021-04-25 17:52:51 -0400

    Add image to LandingZone

--- src/Components/LandingZone/LandingZone.js
+++ src/Components/LandingZone/LandingZone.js
@@ -1,19 +1,26 @@
 import { Button, Container, Row, Col } from 'reactstrap';
 
+import Coin from '../../Assets/LandingZone_Coin.png';
+
 import './landingZone.css';
 
 const LandingZone = () => {
     
     return(
-        <Container className='landing-div' fluid={true}>
-            <Row>
+        <Container id='landing-zone' fluid={true}>
+            <Row id='first-row'>
                 <Col className='text-center'>
-                    <h1>Hey there, my name is <span className='my-name'>Sean Dwyer</span>.</h1>
+                <h1><span><img className='coin' src={Coin} /></span>INSERT COIN</h1>
                 </Col>
             </Row>
             <Row>
                 <Col className='text-center'>
-                    <h1>I'm a full-stack developer.</h1>   
+                    <h1>Hello, my name is <span className='my-name'>Sean Dwyer</span>.</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='text-center'>
+                    <h2>I'm a full-stack developer.</h2>   
                 </Col>
             </Row>
             <Row>
