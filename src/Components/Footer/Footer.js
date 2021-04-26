c37bda9bdf8e5d298c9ad0926c3a467c338a27be
Sean Dwyer <spdwyer06@gmail.com>
2021-04-26 13:13:51 -0400

    Change color of Footer images. Link each Footer image to appropriate link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,6 +5,9 @@ import CodePenLogo from '../../Assets/FooterLogo_CodePen1.png';
 import GitHubLogo from '../../Assets/FooterLogo_GitHub10.png';
 import GitHubLogo2 from '../../Assets/FooterLogo_GitHub2.png';
 import GitHubLogo3 from '../../Assets/FooterLogo_GitHub3.png';
+import LinkedInLogo2 from '../../Assets/FooterLogo_LinkedIn2.png';
+import CodePenLogo2 from '../../Assets/FooterLogo_CodePen2.png';
+import CodePenLogo3 from '../../Assets/FooterLogo_CodePen3.png';
 
 import './footer.css';
 
@@ -19,13 +22,16 @@ const Footer = () => {
             </Row>
             <Row>
                 <Col md='4'>
-                    <img className='footer-img' src={LinkedInLogo} alt='LinkedIn' />
+                    <a href='https://www.linkedin.com/in/spdwyer3/' target='_blank'><img className='footer-img' src={LinkedInLogo2} alt='LinkedIn' /></a>
+                    {/* <img className='footer-img' src={LinkedInLogo2} alt='LinkedIn' /> */}
                 </Col>
                 <Col md='4'>
-                    <img className='footer-img' src={CodePenLogo} alt='CodePen' />
+                    <a href='https://codepen.io/dark_thorns/pens/public' target='_blank'><img className='footer-img' src={CodePenLogo3} alt='CodePen' /></a>
+                    {/* <img className='footer-img' src={CodePenLogo3} alt='CodePen' /> */}
                 </Col>
                 <Col md='4'>
-                    <img className='footer-img' src={GitHubLogo} alt='GitHub' />
+                    <a href='https://github.com/spdwyer06' target='_blank'><img className='footer-img' src={GitHubLogo} alt='GitHub' /></a>
+                    {/* <img className='footer-img' src={GitHubLogo} alt='GitHub' /> */}
                 </Col>
             </Row>
             <Row>
